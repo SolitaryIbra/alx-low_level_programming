62034c0c22761e18fb5798ce21ac03f6c9381504
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-18 09:14:08 +0200

    Create 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+* main - function extracts the last number in an integer and checks if greater than 5 or else smaller or equal to 0
+*
+* Description: In main function checks the value of the last nummebr by moduling 
+* the initial number and checks if it is greater than the current number
+* Return = 0
+*
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+  int last_num = n % 10;
+
+  printf("Last digit of %d is %d ",n ,last_num);
+  if (last_num > 5)
+  {
+      printf("and is greater than 5\n");
+  }
+  else if (last_num < 5)
+  {
+      printf("and is less than 6 and not 0\n");
+  }
+  else if (last_num == 0)
+  {
+      printf("and is 0\n");
+  }
+	return (0);
+}
