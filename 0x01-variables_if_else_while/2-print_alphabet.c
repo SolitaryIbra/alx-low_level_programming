cfed24dbcb9e2d07a496aaad2de35e26c84f461f
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-18 11:40:00 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
- * main - functio nto print the alphabet in lower case.
- * 
- * Description: This function is only main to print the 
- * alpahabet in new line lower-cased
- * Return: 0
+* main - functio nto print the alphabet in lower case.
+* 
+* Description: This function is only main to print the 
+* alpahabet in new line lower-cased
+* Return: 0
 */
 int main(void){
-    char alpahbet[26] = "abcdefghijklmnopqrstuvwxyz";
+char alpahbet[26] = "abcdefghijklmnopqrstuvwxyz";
 
-    for(int i = 0; i<26; i++){
-        putchar(alpahbet[i]);
-        putchar('\n');
-    }
-    return (0);
+int i = 0;
+while(i<26){
+    putchar(alpahbet[i]);
+    putchar('\n');
+    i++
+}
+return (0);
 }
