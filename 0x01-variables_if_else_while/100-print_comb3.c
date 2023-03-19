2a29c4b7df1bc755d4d86cf47e1c03e5b2db98de
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-19 23:59:21 +0200

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+* main - This main of the program
+*
+* Description: This program prints the required possable combination
+* of numbers from 0 to 10 without repeating the same number.
+* Return: 0
+*/
+int main(void)
+{
+
+int i = 48;
+int j = 48;
+
+while (i < 58)
+{
+      int j = i+1;
+
+      while(j < 58)
+      {
+         putchar(i);
+         putchar(j);
+
+         if(i != 56)
+         {
+            putchar(',');
+            putchar(' ');
+         }
+
+         j++;
+      }
+
+   i++;
+   j++;
+   }
+
+return (0);
+}
