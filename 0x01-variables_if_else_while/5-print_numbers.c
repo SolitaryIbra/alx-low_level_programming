4682711e29b3dbec4dcb1a5987277f4aea406eb2
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-19 22:56:57 +0200

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*
+* main - This code prints the numbers in base 10
+*
+* Description: This code prints the sorted alphabet
+* except the letters e and q
+* Return: 0
+*
+*/
+int main(void){
+
+int i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+
+return (0);
+}
