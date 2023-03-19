f223e9e3925ce594f140b8c984565813cc2419be
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-20 00:12:19 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,14 @@ int j = i;
 
 while (i < 58)
 {
-      int j = i+1;
+      int j = i + 1;
 
-      while(j < 58)
+      while (j < 58)
       {
          putchar(i);
          putchar(j);
 
-         if(i != 56)
+         if (i != 56)
          {
             putchar(',');
             putchar(' ');
