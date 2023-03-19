4c623d278ebea151cec2f8a9711e89703eeca573
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-19 22:35:03 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 
 /**
-*
 * main - This code prints the alphabet in 
 * lower case except e and q letters
 *
 * Description: This code prints the sorted alphabet
 * except the letters e and q
 * Return: 0
-*
 */
-int main(void)
-{
-char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int main(void){
+
+int ch = 97;
 int i = 0;
+
 while (i < 26)
 {
-if (alphabet[i] == 'e' || alphabet[i] == 'q')
-{
-continue;
+if(i != 4 && i != 16){
+putchar(ch);
 }
-else
-{
-putchar(alphabet[i]);      
-}
+ch++;
 i++;
 }
+
 return (0);
 }
