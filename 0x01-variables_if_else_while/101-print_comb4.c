08c8b1a469ccfd3a079a7dc83e43ddacbefac335
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-20 11:49:13 +0200

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - This program Prints the combination of all numbers in three
+* 
+* Description: This is how to print thhree numbers in a consecuative
+* order without doing that again in different order
+* Return: 0
+*/
+int main(void)
+{
+int i = 48;
+int j, k;
+while (i < 58)
+{
+j = i + 1;
+
+while(j < 58)
+{
+k = j + 1;
+
+while (k < 58)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (k != 58)
+{
+putchar(',');
+putchar(' ');
+}
+k++;
+}
+j++;
+}
+
+i++;
+}
+return (0);
+}
