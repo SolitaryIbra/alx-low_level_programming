e426c869b9d80d48ed9b61fa81a627839b566b83
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-22 01:28:04 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
 * print_alphabet_x10 - prints the alphabet sorted 10 times
 */
 void print_alphabet_x10(void)
@@ -9,7 +9,7 @@ int j = 0;
 while (j < 10)
 {
 int i = 97;
-while (i < (97+26))
+while (i < (97 + 26))
 {
 putchar(i);
 i++;
