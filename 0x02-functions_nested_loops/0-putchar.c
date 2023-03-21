d8cc5f1a27f7be1f207d75920d01c263f7951235
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-03-21 06:04:17 +0200

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - This code prints "_putcahr"
+* Description: This code prints "_putchar" using
+* printf() method
+* Return: 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
