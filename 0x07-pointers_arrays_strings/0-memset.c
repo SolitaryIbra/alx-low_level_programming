1bfd2c107ffc9255863538c8eca555ab15925c04
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-04-04 03:03:31 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+/**
+* _memset - This function fills a speecific place in memory.
+* Return: Of type pointer to another address.
+*/
+char* _memset(char* fill, char b, unsigned int lim)
+{
+	unsigned int num = 0;
+
+	while (num < lim)
+	{
+	fill[num] = b;
+	num++;
+	}
+
+	return (fill);
+}
