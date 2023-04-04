339bd59f579eb6d7f8fccb2b7bb43e30a19b2a81
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-04-04 03:18:42 +0200

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memcpy - This function copies number of bytes from memory pointer
+* to memory of another pointer.
+*
+* Return: Of type pointer to another pointer
+*/
+char *_memcpy(char* to_ptr, char* from_ptr, unsigned int n)
+{
+	unsigned int num = 0;
+
+	while (num < n)
+	{
+		to_ptr[num] = from_ptr[num];
+		num++;
+	}
+
+	return (from_ptr);
+}
