1fb99fa6847173718965053d445e79f3042c576f
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-05-01 04:49:29 +0200

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * factorial - factorial of num
- * @num: an integer
- * Return: num
- */
+* factorial - factorial of num
+* @num: an integer
+* Return: num
+*/
 int factorial(int num)
 {
-	int fac;
-	if (num == 0)
-	{
-		return (1);
-	}
-	else if (num < 0)
-	{
-		return (-1);
-	}
-	else
-	{
-		fac = num * factorial(num - 1);
-	}
-			return (fac);
+int fac;
+	
+if (num == 0)
+{
+return (1);
+}
+else if (num < 0)
+{
+return (-1);
+}
+else
+{
+fac = num * factorial(num - 1);
+}
+return (fac);
 }
