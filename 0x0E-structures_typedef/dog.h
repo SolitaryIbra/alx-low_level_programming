411e0707088e7628a2b0794c6f5a385642e5f141
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-05-02 12:17:38 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,13 +17,13 @@ float age;
 char *owner;
 };
 
-void init_dog(struct dog*, char*, float, char*);
-void print_dog(struct dog*);
+void init_dog(struct dog *, char *, float, char*);
+void print_dog(struct dog *);
 
 typedef struct dog dog_t;
 
-dog_t *new_dog(char*, float, char*);
+dog_t *new_dog(char *, float, char *);
 
-void free_dog(dog_t *d);
+void free_dog(dog_t *);
 
 #endif /* _DOG_H_ */
