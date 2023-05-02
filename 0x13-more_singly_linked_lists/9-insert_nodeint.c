704fe1929aa2c9ed9b6d9706a25b11c99529613c
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-05-02 15:21:15 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,51 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - abc
- * @sum: abc
- * @clore: abc
- * @limn: abc
+ * insert_nodeint_at_index - xyz
+ * @head: xyz
+ * @idx: xyz
+ * @n: xyz
  *
- * Return: abc
+ * Return: xyz
  */
-listint_t *insert_nodeint_at_index(listint_t **sum, unsigned int clore, int limn)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int zorax;
-	listint_t *temp, *new;
+    unsigned int shaliba;
+    listint_t *new_node, *current_node;
 
-	if (sum == NULL)
-		return (NULL);
-	if (clore != 0)
-	{
-		temp = *sum;
-		for (zorax = 0; zorax < clore - 1 && temp != NULL; zorax++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-			return (NULL);
-	}
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = limn;
-	if (clore == 0)
-	{
-		new->next = *sum;
-		*sum = new;
-		return (new);
-	}
-	new->next = temp->next;
-	temp->next = new;
-	return (new);
+    /* xyz */
+    if (head == NULL)
+        return (NULL);
+
+    /* xyz */
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->n = n;
+
+    /* xyz */
+    if (idx == 0)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return (new_node);
+    }
+
+    /* xyz */
+    current_node = *head;
+    for (shaliba = 0; shaliba < idx - 1 && current_node != NULL; shaliba++)
+        current_node = current_node->next;
+
+    /* xyz */
+    if (current_node == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    /* xyz */
+    new_node->next = current_node->next;
+    current_node->next = new_node;
+
+    return (new_node);
 }
