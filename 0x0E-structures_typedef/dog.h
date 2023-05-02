ab400a11c2f5f234cccccdcfca292d023a98b03b
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-05-02 12:12:19 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,19 +2,19 @@
 #define _DOG_H_
 
 /**
-* struct dog - dog class
-*
-* @name: Dog Name
-*
-* @age: Dog Age
-*
-* @owner: Dog Owner
-*/
+ * struct dog - dog class
+ *
+ * @name: Dog Name
+ *
+ * @age: Dog Age
+ *
+ * @owner: Dog Owner
+ */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
 void init_dog(struct dog*, char*, float, char*);
@@ -26,4 +26,4 @@ dog_t *new_dog(char*, float, char*);
 
 void free_dog(dog_t *d);
 
-#endif /*_DOG_H*/
+#endif /* _DOG_H_ */
