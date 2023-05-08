4ca9d8f94da6a8f47957048f775788d23bc33c7c
Ibrahim Mohamed <120879381+SolitaryIbra@users.noreply.github.com>
2023-05-08 18:43:38 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,8 +8,8 @@
 #include <fcntl.h>
 
 int _putchar(char);
-ssizee_t read_textfile(const char *, size_t);
+ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
 int append_text_to_file(const char *, char *);
 
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
